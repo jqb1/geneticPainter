fcc5f109da1f715d1545649ffded722ec34a127e
jqb1 <jakub.koziol12@gmail.com>
2019-11-06 01:06:10 +0100

    generating population, crossover 2 childs

--- figures.py
+++ figures.py
@@ -6,8 +6,9 @@ import numpy as np
 import pygame
 
 from painting import Painting
-
+from random import randint, choices
 POPULATION_SIZE = 10
+SHAPES_NUMBER = 50
 
 
 def main():
@@ -15,40 +16,51 @@ def main():
     height, width = image.shape[:2]
     screen = pygame.display.set_mode([width, height])
 
-    new_generation = generate_initial_population(width, height, image)
-    screen.blit(new_generation[0][1], (0, 0))
+    population = generate_initial_population(width, height, image)
+    screen.blit(population[0][1], (0, 0))
     pygame.display.flip()
 
-    # ------------------
-    child = crossover(new_generation[0][2], new_generation[1][2])
-    fitness(image, child.draw())
-    # -------------------
     running = True
     while running:
+        population = generate_new_population(population)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 pygame.display.quit()
 
 
-def generate_new_population():
-    pass
+def generate_new_population(previous_population):
+    def mutate_random_shapes(_painting):
+        for _ in range(randint(3, 10)):
+            selected_shape = _painting[2].shapes[randint(1, SHAPES_NUMBER-1)]
+            selected_shape.rand_color()
+            selected_shape.mutate_vertices()
+        return _painting[2]
+
+    new_population = []
+    new_population.extend(crossover(previous_population[0][2], previous_population[1][2]))
+    for painting in previous_population[2:POPULATION_SIZE]:
+        new_population.append(mutate_random_shapes(painting))
+    return new_population
+    # fitness(image, child.draw()) !!!!!!!!!!!!!!!
 
 
 def crossover(parent1, parent2):
-    child = Painting(100, parent1.width, parent2.height)
-    child.shapes = parent1.shapes[:49] + parent2.shapes[50:99]
-    return child
+    child1 = Painting(SHAPES_NUMBER, parent1.width, parent1.height)
+    child2 = Painting(SHAPES_NUMBER, parent1.width, parent1.height)
+    child1.shapes = parent1.shapes[:49] + parent2.shapes[50:99]
+    child2.shapes = parent1.shapes[50:99] + parent2.shapes[:49]
+    return child1, child2
 
 
-def mutation():
+def mutation(painting):
     pass
 
 
 def generate_initial_population(width, height, image):
     paintings = []
     for _ in range(POPULATION_SIZE):
-        painting = Painting(100, width, height)
+        painting = Painting(SHAPES_NUMBER, width, height)
         painting.create_init_shapes()
         paintings.append(painting)
 
--- shape.py
+++ shape.py
@@ -6,16 +6,23 @@ class Shape:
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.vertices = []
-        self.color = self._rand_color()
+        self.color = self.rand_color()
 
     def set_init_vertices(self) -> None:
         self.vertices = [
-            (randint(1, self.screen_width), randint(1, self.screen_height)),
-            (randint(1, self.screen_width), randint(1, self.screen_height)),
-            (randint(1, self.screen_width), randint(1, self.screen_height))
+            [randint(1, self.screen_width), randint(1, self.screen_height)],
+            [randint(1, self.screen_width), randint(1, self.screen_height)],
+            [randint(1, self.screen_width), randint(1, self.screen_height)]
         ]
 
     @staticmethod
-    def _rand_color() -> tuple:
+    def rand_color() -> tuple:
         # RGB, alpha
         return randint(1, 255), randint(1, 255), randint(1, 255), randint(1, 180)
+
+    def mutate_vertices(self):
+        for vertice in self.vertices:
+            if vertice[0] % 2 == 0:
+                (vertice[0] + randint(1, self.screen_width // 20), vertice[1] + randint(1, self.screen_height // 20))
+            else:
+                (vertice[0] - randint(1, self.screen_width // 20), vertice[1] - randint(1, self.screen_height // 20))
